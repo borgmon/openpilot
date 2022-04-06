ceac9e204e71873dcf9adc1cabb5e08c56cbe9af
borgmon <borgmon7@gmail.com>
2022-04-06 00:20:30 -0700

    add vision turn

--- selfdrive/ui/qt/onroad.h
+++ selfdrive/ui/qt/onroad.h
@@ -53,6 +53,9 @@ public:
 private:
   void drawIcon(QPainter &p, int x, int y, QPixmap &img, QBrush bg, float opacity);
   void drawText(QPainter &p, int x, int y, const QString &text, int alpha = 255);
+  void drawCenteredText(QPainter &p, int x, int y, const QString &text, QColor color);
+  void drawVisionTurnControllerUI(QPainter &p, int x, int y, int size, const QColor &color, const QString &speed,
+                                  int alpha);
   void paintEvent(QPaintEvent *event) override;
 
   QPixmap engage_img;
@@ -67,6 +70,10 @@ private:
   bool dmActive = false;
   bool hideDM = false;
   int status = STATUS_DISENGAGED;
+
+  bool showVTC = false;
+  QString vtcSpeed;
+  QColor vtcColor;
 
 signals:
   void valueChanged();
@@ -103,6 +110,9 @@ protected:
   void drawLaneLines(QPainter &painter, UIState *s);
   void drawLead(QPainter &painter, const cereal::ModelDataV2::LeadDataV3::Reader &lead_data, const QPointF &vd);
   inline QColor redColor(int alpha = 255) { return QColor(201, 34, 49, alpha); }
+  inline QColor blackColor(int alpha = 255) { return QColor(0, 0, 0, alpha); }
+  inline QColor greenColor(int alpha = 255) { return QColor(49, 201, 34, alpha); }
+  inline QColor graceBlueColor(int alpha = 255) { return QColor (34, 49, 201, alpha); }
   double prev_draw_t = 0;
 
 signals:
